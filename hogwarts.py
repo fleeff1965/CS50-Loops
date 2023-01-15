d447a3672c0a2da0577de9c5ec32b04ee244267b
fleeff1965 <fleeff@gmail.com>
2023-01-15 11:59:06 +0100

    Dict (Dictionary) and for loops

--- hogwarts.py
+++ hogwarts.py
@@ -1,5 +1,12 @@
-students =["Hermione","Harry","Ron"]
+# students = ["Hermione", "Harry", "Ron", "Draco"]
+# houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
 
-n=len(students)
-for i in range(n):
-    print(i+1, students[i])
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep=", ")

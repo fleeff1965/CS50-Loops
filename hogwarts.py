52bfc1eba4c739d8a6b65a6867bbf8edab0328aa
fleeff1965 <fleeff@gmail.com>
2023-01-15 12:19:09 +0100

    List of dict and for loop

--- hogwarts.py
+++ hogwarts.py
@@ -1,12 +1,13 @@
 # students = ["Hermione", "Harry", "Ron", "Draco"]
 # houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-}
+students = [
+    {"name" : "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name" : "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name" : "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name" : "Draco", "house": "Slytherin", "patronus": None}
+]
+
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], student["house"], student["patronus"], sep=", ")
